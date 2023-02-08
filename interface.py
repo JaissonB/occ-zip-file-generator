f8c78f4ca38a7ff15417ba63d3c987f7ef281549
JaissonB <097319@aluno.uricer.edu.br>
2023-02-08 01:40:30 -0300

    Add element paths

--- interface.py
+++ interface.py
@@ -1,5 +1,9 @@
 from PySimpleGUI import PySimpleGUI as sg
 from pathlib import Path
+from zipfile import ZipFile
+import os
+from os.path import basename
+from shutil import rmtree
 root = Path().absolute()
 ap = '"'
 chi = '{'
@@ -9,7 +13,8 @@ sg.theme('Reddit')
 layout = [
     [sg.Text('Widget Name: '), sg.Input(key='widgetName')],
     [sg.Text('Extension ID: '), sg.Input(key='extensionId')],
-    [sg.Text('Elements: '), sg.Checkbox('')],
+    [sg.Text('Elements: '), sg.Input(key='elements')],
+    [sg.Text('/Os elements devem ser separados apenas por espaço/')],
     [sg.Text('Date (yyyy-mm-dd): '), sg.Input(key='date')],
     [sg.Button('Gerar arquivo')],
 ]
@@ -20,7 +25,7 @@ def returnExtJsonContent(extensionID, widgetName, date):
     return f"{chi}\n  {ap}createdBy{ap}: {ap}Compasso{ap},\n  {ap}developerID{ap}: {ap}compasso{ap},\n  {ap}extensionID{ap}: {ap}{extensionID}{ap},\n  {ap}translations{ap}: [\n    {chi}\n      {ap}language{ap}: {ap}en{ap},\n      {ap}name{ap}: {ap}{widgetName}{ap},\n      {ap}description{ap}: {ap}{widgetName}{ap}\n    {chf},\n   {chi}\n      {ap}language{ap}: {ap}pt_BR{ap},\n      {ap}name{ap}: {ap}{widgetName}{ap},\n      {ap}description{ap}: {ap}{widgetName}{ap}\n    {chf},\n  ],\n  {ap}timeCreated{ap}: {ap}{date}{ap},\n  {ap}version{ap}: 1\n{chf}"
 
 def returnWidgetJsonContent(widgetName):
-    return f"{chi}\n  {ap}config{ap}: {chi}\n	{ap}defaultPropertyExample{ap}: {ap}widgetName{ap}\n  {chf},\n  {ap}defaultLayout{ap}: {ap}widgetName{ap},\n  {ap}availableToAllPages{ap}: true,\n  {ap}globalEnabled{ap}: true,\n  {ap}i18nresources{ap}: {ap}widgetName{ap},\n  {ap}imports{ap}: [],\n  {ap}javascript{ap}: {ap}widgetName{ap},\n  {ap}jsEditable{ap}: true,\n  {ap}minWidth{ap}: 1,\n  {ap}translations{ap}: [{chi}\n    {ap}language{ap}: {ap}en{ap},\n    {ap}name{ap}: {ap}widgetName{ap}\n  {chf},\n  {chi}\n    {ap}language{ap}: {ap}pt_BR{ap},\n    {ap}name{ap}: {ap}widgetName{ap}\n  {chf}],\n  {ap}version{ap}: 1,\n  {ap}widgetFamily{ap}: {ap}widgetName{ap},\n  {ap}widgetType{ap}: {ap}widgetName{ap}\n{chf}"
+    return f"{chi}\n  {ap}config{ap}: {chi}\n	{ap}defaultPropertyExample{ap}: {ap}{widgetName}{ap}\n  {chf},\n  {ap}defaultLayout{ap}: {ap}{widgetName}{ap},\n  {ap}availableToAllPages{ap}: true,\n  {ap}globalEnabled{ap}: true,\n  {ap}i18nresources{ap}: {ap}{widgetName}{ap},\n  {ap}imports{ap}: [],\n  {ap}javascript{ap}: {ap}{widgetName}{ap},\n  {ap}jsEditable{ap}: true,\n  {ap}minWidth{ap}: 1,\n  {ap}translations{ap}: [{chi}\n    {ap}language{ap}: {ap}en{ap},\n    {ap}name{ap}: {ap}{widgetName}{ap}\n  {chf},\n  {chi}\n    {ap}language{ap}: {ap}pt_BR{ap},\n    {ap}name{ap}: {ap}{widgetName}{ap}\n  {chf}],\n  {ap}version{ap}: 1,\n  {ap}widgetFamily{ap}: {ap}{widgetName}{ap},\n  {ap}widgetType{ap}: {ap}{widgetName}{ap}\n{chf}"
 
 def returnConfigJsonContent(widgetName):
     return f"{chi}\n  {ap}widgetDescriptorName{ap}: {ap}{widgetName}{ap},\n  {ap}properties{ap}: []\n{chf}"
@@ -28,7 +33,13 @@ def returnConfigJsonContent(widgetName):
 def returnLocalesConfigContent():
     return f"{chi}\n  {ap}resources{ap}: {chi}{chf}\n{chf}"
 
-def returnMainJsContent():
+def returnElementTemplateContent():
+    return f"<h2 data-bind={ap}widgetLocaleText : '_comment'{ap}></h2>"
+
+def returnElementJsonContent(widgetName, elementName):
+    return f"{chi}\n  {ap}availableToAllWidgets{ap}: false,\n  {ap}configOptions{ap}: [],\n  {ap}global{ap}: false,\n  {ap}inline{ap}: false,\n  {ap}supportedWidgetType{ap}: [{ap}{widgetName}{ap}],\n  {ap}tag{ap}: {ap}{elementName}{ap},\n  {ap}translations{ap}: [\n    {chi}\n      {ap}description{ap}: {ap}{elementName}{ap},\n      {ap}language{ap}: {ap}pt_BR{ap},\n      {ap}title{ap}: {ap}{elementName}{ap}\n    {chf},\n    {chi}\n      {ap}description{ap}: {ap}{elementName}{ap},\n      {ap}language{ap}: {ap}en{ap},\n      {ap}title{ap}: {ap}{elementName}{ap}\n    {chf}\n  ],\n  {ap}type{ap}: {ap}fragment{ap},\n  {ap}version{ap}: 1\n{chf}"
+
+def returnJsContent():
     return f"define(\n    [],\n\n    function () {chi}\n\n        {ap}use strict{ap};\n\n        return {chi}\n\n        {chf};\n    {chf}\n);"
 
 def returnDisplayTemplateContent():
@@ -54,30 +65,29 @@ while True:
         else:
             widgetName = values['widgetName']
             extensionId = values['extensionId']
+            elementsNames = values['elements']
             date = values['date']
             print(widgetName)
             print(extensionId)
-            # print(values[''])
+            print(elementsNames)
             print(date)
             print(root)
-            #cria a pasta principal
-            mainDir = root / widgetName
-            mainDir.mkdir()
+
             #cria a pasta widget
-            widgetDir = mainDir / 'widget'
+            widgetDir = root / 'widget'
             widgetDir.mkdir()
             #cria o ext.json
-            extFile = mainDir / 'ext.json'
+            extFile = root / 'ext.json'
             extFile.touch()
             extFile.write_text(returnExtJsonContent(extensionId, widgetName, date))
             #cria pasta com nome do widget
             widgetNameDir = widgetDir / widgetName
             widgetNameDir.mkdir()
+
             #cria arquivo widget.json
             widgetFile = widgetNameDir / 'widget.json'
             widgetFile.touch()
             widgetFile.write_text(returnWidgetJsonContent(widgetName))
-
 
             #cria a pasta config
             configDir = widgetNameDir / 'config'
@@ -102,6 +112,33 @@ while True:
             elementDir = widgetNameDir / 'element'
             elementDir.mkdir()
 
+            if elementsNames != '':
+                arrayElementsNames = elementsNames.split()
+                for i in range(len(arrayElementsNames)):
+                    #cria pasta do element
+                    elementIndexDir = elementDir / arrayElementsNames[i]
+                    elementIndexDir.mkdir()
+                    #cria arquivo element.json
+                    jsonElementIndexFile = elementIndexDir / 'element.json'
+                    jsonElementIndexFile.touch()
+                    jsonElementIndexFile.write_text(returnElementJsonContent(widgetName, arrayElementsNames[i]))
+
+                    #cria pasta js do element
+                    jsElementIndexDir = elementIndexDir / 'js'
+                    jsElementIndexDir.mkdir()
+                    #cria arquivo js do element
+                    jsElementIndexFile = jsElementIndexDir / 'element.js'
+                    jsElementIndexFile.touch()
+                    jsElementIndexFile.write_text(returnJsContent())
+
+                    #cria pasta templates do element
+                    templateElementIndexDir = elementIndexDir / 'templates'
+                    templateElementIndexDir.mkdir()
+                    #cria arquivo template do element
+                    templateElementIndexFile = templateElementIndexDir / 'template.txt'
+                    templateElementIndexFile.touch()
+                    templateElementIndexFile.write_text(returnElementTemplateContent())
+
             #cria a pasta images
             imagesDir = widgetNameDir / 'images'
             imagesDir.mkdir()
@@ -113,7 +150,7 @@ while True:
             jsName = widgetName + '.js'
             jsFile = jsDir / jsName
             jsFile.touch()
-            jsFile.write_text(returnMainJsContent())
+            jsFile.write_text(returnJsContent())
 
             #cria a pasta layouts
             layoutsDir = widgetNameDir / 'layouts'
@@ -161,6 +198,11 @@ while True:
             displayTemplateFile = templatesDir / 'display.template'
             displayTemplateFile.touch()
             displayTemplateFile.write_text(returnDisplayTemplateContent())
-
-            print(mainDir)
-            print(returnExtJsonContent(extensionId, widgetName, date))
+            
+            with ZipFile(f"{widgetName}.zip", "w") as zip:
+                
+                zip.mkdir("widget")
+                zip.write("ext.json")
+            
+            # rmtree(widgetDir)
+            # extFile.unlink()
